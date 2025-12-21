e2f92c86f010de8dfadfe1197dd6b9821136736a
bala-amypo <bala@amypo.in>
2025-12-21 15:55:02 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -1,5 +1,19 @@
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
 
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Exam Seating Arrangement API")
+                        .description("Swagger UI for Exam Seating Arrangement Generator")
+                        .version("1.0"));
+    }
 }
