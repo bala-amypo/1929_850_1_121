283138d79f508312b293f67705189fe06782b1f1
bala-amypo <bala@amypo.in>
2025-12-21 16:28:38 +0000

    User commit

--- src/main/java/dto/RegisterRequest.java
+++ src/main/java/dto/RegisterRequest.java
@@ -1,5 +1,27 @@
-package dto;
+package com.example.demo.dto;
 
 public class RegisterRequest {
 
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public RegisterRequest() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
 }
