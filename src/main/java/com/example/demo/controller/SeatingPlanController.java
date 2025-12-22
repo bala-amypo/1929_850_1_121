737616c85632c887dbe770d11ce9a1c39b56eb9c
bala-amypo <bala@amypo.in>
2025-12-22 02:47:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/SeatingPlanController.java
+++ src/main/java/com/example/demo/controller/SeatingPlanController.java
@@ -2,7 +2,6 @@ package com.example.demo.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Collections;
