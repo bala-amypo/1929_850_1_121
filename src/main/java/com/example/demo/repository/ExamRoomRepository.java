a0f7955fd3925150719b5abcd9fba9d16bac0e1e
bala-amypo <bala@amypo.in>
2025-12-22 08:10:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++ src/main/java/com/example/demo/repository/ExamRoomRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.ExamRoom;
+import com.example.demo.model.ExamRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
