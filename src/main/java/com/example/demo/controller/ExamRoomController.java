e9d05df862b5af8e43b03e328262ccadbc6051f3
bala-amypo <bala@amypo.in>
2025-12-22 05:46:18 +0000

    User commit

--- src/main/java/com/example/demo/controller/ExamRoomController.java
+++ src/main/java/com/example/demo/controller/ExamRoomController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.ExamRoom;
+import com.example.demo.service.ExamRoomService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,9 +12,18 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Exam Rooms")
 public class ExamRoomController {
 
+    @Autowired
+    private ExamRoomService examRoomService;
+
     @GetMapping
     @Operation(summary = "List rooms")
     public String rooms() {
         return "Rooms API";
+    }
+
+    @PostMapping
+    @Operation(summary = "Create exam room")
+    public ExamRoom createRoom(@RequestBody ExamRoom room) {
+        return examRoomService.saveRoom(room);
     }
 }
