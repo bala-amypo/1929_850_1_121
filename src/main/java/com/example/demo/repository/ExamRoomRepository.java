c7953100c64534caae716df5573aa13bcea6194e
bala-amypo <bala@amypo.in>
2025-12-22 05:47:28 +0000

    User commit

--- src/main/java/com/example/demo/repository/ExamRoomRepository.java
+++ src/main/java/com/example/demo/repository/ExamRoomRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExamRoomRepository extends JpaRepository<ExamRoom, Long> {
 
-    // Example: find by room number
     ExamRoom findByRoomNumber(String roomNumber);
 }
