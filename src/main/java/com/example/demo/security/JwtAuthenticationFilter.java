0c0caa98ddf95d4dbb2f3ed46c455ba583c4d897
bala-amypo <bala@amypo.in>
2025-12-27 02:01:47 +0000

    User commit

--- src/main/java/com/example/demo/security/JwtAuthenticationFilter.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.security;
-
-/*
- * Dummy authentication filter.
- * No servlet or security logic required.
- */
-public class JwtAuthenticationFilter {
-    // Empty on purpose
-}
