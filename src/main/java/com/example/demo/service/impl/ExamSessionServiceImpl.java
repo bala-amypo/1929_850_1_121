03a5c74f92b69603e62af36eaa914411922fd87d
bala-amypo <bala@amypo.in>
2025-12-27 01:11:23 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ExamSessionServiceImpl.java
@@ -3,5 +3,5 @@ package com.example.demo.service;
 import com.example.demo.model.ExamSession;
 
 public interface ExamSessionService {
-    ExamSession saveExamSession(ExamSession session);
+    ExamSession save(ExamSession session);
 }
