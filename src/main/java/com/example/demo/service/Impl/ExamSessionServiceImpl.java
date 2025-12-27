8bae950eb050d77c20cc24d05e869d8d5bddd547
bala-amypo <bala@amypo.in>
2025-12-27 00:55:48 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ExamSessionServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ExamSessionServiceImpl.java
@@ -1,28 +1,7 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.model.ExamSession;
-import com.example.demo.repository.ExamSessionRepository;
-import com.example.demo.service.ExamSessionService;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-@Service
-public class ExamSessionServiceImpl implements ExamSessionService {
-
-    private final ExamSessionRepository repository;
-
-    public ExamSessionServiceImpl(ExamSessionRepository repository) {
-        this.repository = repository;
-    }
-
-    @Override
-    public List<ExamSession> getAllExamSessions() {
-        return repository.findAll();
-    }
-
-    @Override
-    public ExamSession getExamSessionById(long id) {
-        return repository.findById(id).orElse(null);
-    }
+public interface ExamSessionService {
+    ExamSession saveExamSession(ExamSession session);
 }
