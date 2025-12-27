70c7c4de91d1512b592f7337ca30322a4f0fc7df
bala-amypo <bala@amypo.in>
2025-12-27 01:25:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,26 +1,7 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.Student;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class StudentRepository {
-    private final List<Student> students = new ArrayList<>();
-
-    public Optional<Student> findById(Long id) {
-        return students.stream().filter(s -> s.getId().equals(id)).findFirst();
-    }
-
-    public List<Student> findAll() {
-        return students;
-    }
-
-    public Student save(Student student) {
-        students.add(student);
-        return student;
-    }
+public interface StudentRepository extends JpaRepository<Student, Long> {
 }
