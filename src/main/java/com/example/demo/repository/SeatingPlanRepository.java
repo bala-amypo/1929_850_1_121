07bae2c972c9c13df1072acf2a071739bf82b01c
bala-amypo <bala@amypo.in>
2025-12-28 19:05:02 +0000

    User commit

--- src/main/java/com/example/demo/repository/SeatingPlanRepository.java
+++ src/main/java/com/example/demo/repository/SeatingPlanRepository.java
@@ -1,9 +1,15 @@
-package com.example.demo.repository; 
-import java.util.List; 
-import org.springframework.data.jpa.repository.JpaRepository; 
-import org.springframework.stereotype.Repository; 
-import com.example.demo.model.SeatingPlan; 
-@Repository 
-public interface SeatingPlanRepository extends JpaRepository<SeatingPlan,Long>{ 
-List<SeatingPlan> findByExamSessionId(Long sessionId); 
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.SeatingPlan;
+
+@Repository
+public interface SeatingPlanRepository extends JpaRepository<SeatingPlan,Long>{
+
+    List<SeatingPlan> findByExamSessionId(Long sessionId);
+
 }
--- src/main/java/com/example/demo/service/ExamRoomService.java
+++ src/main/java/com/example/demo/service/ExamRoomService.java
@@ -1,16 +1,10 @@
- 
-package com.example.demo.service; 
- 
-import java.util.List; 
- 
-import com.example.demo.model.ExamRoom; 
- 
-public interface ExamRoomService{ 
-    ExamRoom addRoom(ExamRoom room); 
-    List<ExamRoom> getAllRooms(); 
-} 
+package com.example.demo.service;
 
+import java.util.List;
 
+import com.example.demo.model.ExamRoom;
 
- 
-
+public interface ExamRoomService{
+    ExamRoom addRoom(ExamRoom room);
+    List<ExamRoom> getAllRooms();
+}
