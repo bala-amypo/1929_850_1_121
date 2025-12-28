e7216dca7a15339204c7dd34be8d7fb73da28617
bala-amypo <bala@amypo.in>
2025-12-28 18:25:02 +0000

    User commit

--- src/main/java/com/example/demo/model/Student.java
+++ src/main/java/com/example/demo/model/Student.java
@@ -1,31 +1,31 @@
-package com.example.demo.model;
-
-import java.util.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Table(name = "students")
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class Student {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column(unique=true, nullable = false)
-    private String rollNumber;
-    
-    private String name;
-    private String department;
-    private Integer year;
-
-    @Builder.Default
-    @ManyToMany(mappedBy = "students")
-    @JsonIgnore
-    private Set<ExamSession> examSessions = new HashSet<>();
+package com.example.demo.model; 
+ 
+import java.util.*; 
+import com.fasterxml.jackson.annotation.JsonIgnore; 
+import jakarta.persistence.*; 
+import lombok.*; 
+ 
+@Entity 
+@Table(name = "students") 
+@Getter 
+@Setter 
+@Builder 
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class Student { 
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY) 
+    private Long id; 
+     
+    @Column(unique=true, nullable = false) 
+    private String rollNumber; 
+     
+    private String name; 
+    private String department; 
+    private Integer year; 
+ 
+    @Builder.Default 
+    @ManyToMany(mappedBy = "students") 
+    @JsonIgnore 
+    private Set<ExamSession> examSessions = new HashSet<>(); 
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,33 +1,33 @@
-package com.example.demo.model;
-
-import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import lombok.*;
-
-@Entity
-@Table(name="users")
-@Getter 
-@Setter
-@Builder
-@NoArgsConstructor 
-@AllArgsConstructor 
-public class User {
-
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-    @Email
-    @NotBlank
-    @Column(unique=true)
-    private String email;
-
-    @NotBlank
-    private String password;
-
-    @Builder.Default 
-    private String role = "STAFF";
-}
+package com.example.demo.model; 
+ 
+import jakarta.persistence.*; 
+import jakarta.validation.constraints.Email; 
+import jakarta.validation.constraints.NotBlank; 
+import lombok.*; 
+ 
+@Entity 
+@Table(name="users") 
+@Getter  
+@Setter 
+@Builder 
+@NoArgsConstructor  
+@AllArgsConstructor  
+public class User { 
+ 
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY) 
+    private Long id; 
+ 
+    private String name; 
+ 
+    @Email 
+    @NotBlank 
+    @Column(unique=true) 
+    private String email; 
+ 
+    @NotBlank 
+    private String password; 
+ 
+    @Builder.Default  
+    private String role = "STAFF"; 
+} 
