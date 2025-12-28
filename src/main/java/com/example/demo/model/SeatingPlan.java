127b4b327154fee1042c0922581aee37bb7158cb
bala-amypo <bala@amypo.in>
2025-12-28 18:22:02 +0000

    User commit

--- src/main/java/com/example/demo/model/SeatingPlan.java
+++ src/main/java/com/example/demo/model/SeatingPlan.java
@@ -1,39 +1,39 @@
-package com.example.demo.model;
-
-import java.time.LocalDateTime;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.*;
-import lombok.*;
-
-@Entity
-@Table(name = "seating_plans")
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class SeatingPlan {
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JsonIgnoreProperties({"students", "hibernateLazyInitializer", "handler"}) 
-    private ExamSession examSession;
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-    private ExamRoom room;
-
-    @Column(columnDefinition = "TEXT")
-    private String arrangementJson;
-    
-    private LocalDateTime generatedAt;
-
-    @PrePersist
-    public void prePersist() {
-        if (generatedAt == null) {
-            generatedAt = LocalDateTime.now();
-        }
-    }
-}
+package com.example.demo.model; 
+ 
+import java.time.LocalDateTime; 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties; 
+import jakarta.persistence.*; 
+import lombok.*; 
+ 
+@Entity 
+@Table(name = "seating_plans") 
+@Getter 
+@Setter 
+@Builder 
+@NoArgsConstructor 
+@AllArgsConstructor 
+public class SeatingPlan { 
+    @Id 
+    @GeneratedValue(strategy=GenerationType.IDENTITY) 
+    private Long id; 
+ 
+    @ManyToOne(fetch = FetchType.EAGER) 
+    @JsonIgnoreProperties({"students", "hibernateLazyInitializer", "handler"})  
+    private ExamSession examSession; 
+ 
+    @ManyToOne(fetch = FetchType.EAGER) 
+    @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"}) 
+    private ExamRoom room; 
+ 
+    @Column(columnDefinition = "TEXT") 
+    private String arrangementJson; 
+     
+    private LocalDateTime generatedAt; 
+ 
+    @PrePersist 
+    public void prePersist() { 
+        if (generatedAt == null) { 
+            generatedAt = LocalDateTime.now(); 
+        } 
+    } 
+} 
