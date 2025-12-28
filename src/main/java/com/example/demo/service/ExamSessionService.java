c35fbc8076ab91e0f7e8f079b800b5af7f26e6c4
bala-amypo <bala@amypo.in>
2025-12-28 16:37:02 +0000

    User commit

--- src/main/java/com/example/demo/service/ExamSessionService.java
+++ src/main/java/com/example/demo/service/ExamSessionService.java
@@ -2,17 +2,8 @@ package com.example.demo.service;
 
 import com.example.demo.model.ExamSession;
 
-import java.time.LocalDate;
-import java.util.List;
 
-public interface ExamSessionService {
-
+public interface ExamSessionService{
     ExamSession createSession(ExamSession session);
-
-    ExamSession getSession(Long id);
-
-    // ✅ tests expect list
-    List<ExamSession> getSessionsByDate(LocalDate date);
-
-    List<ExamSession> getAllSessions();
+    ExamSession getSession(Long sessionId);
 }
