9a6226ea359d63e29897424370624504e2e68ef8
bala-amypo <bala@amypo.in>
2025-12-28 19:02:02 +0000

    User commit

--- src/main/java/com/example/demo/model/ExamRoom.java
+++ src/main/java/com/example/demo/model/ExamRoom.java
@@ -1,34 +1,34 @@
-package com.example.demo.model; 
- 
-import jakarta.persistence.*; 
-import lombok.*; 
- 
-@Entity 
-@Table(name = "exam_rooms") 
-@Getter 
-@Setter 
-@Builder 
-@NoArgsConstructor 
-@AllArgsConstructor 
-public class ExamRoom { 
-    @Id 
-    @GeneratedValue(strategy=GenerationType.IDENTITY) 
-    private Long id; 
-     
-    @Column(unique = true) 
-    private String roomNumber; 
-     
-    @Column(name = "room_rows") 
-    private Integer rows; 
-     
-    @Column(name = "room_columns") 
-    private Integer columns; 
-     
-    private Integer capacity; 
- 
-    public void ensureCapacityMatches() { 
-        if (this.rows != null && this.columns != null) { 
-            this.capacity = this.rows * this.columns; 
-        } 
-    } 
-} 
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "exam_rooms")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExamRoom {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(unique = true)
+    private String roomNumber;
+    
+    @Column(name = "room_rows")
+    private Integer rows;
+    
+    @Column(name = "room_columns")
+    private Integer columns;
+    
+    private Integer capacity;
+
+    public void ensureCapacityMatches() {
+        if (this.rows != null && this.columns != null) {
+            this.capacity = this.rows * this.columns;
+        }
+    }
+}
